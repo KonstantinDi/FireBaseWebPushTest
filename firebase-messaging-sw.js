254566f84fcd94db8b2c3cb5b38be9b06258ff75
Konstantin Fedorov <konstantinDigit@gmail.com>
2022-03-30 20:32:06 +0300

    try to go on

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,28 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
 
-firebase.initializeApp({
-    "messagingSenderId":"496386485511"
-})
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCsMpfd2oFVp3vVkM_R2EoscmNIMlHciOc",
+    authDomain: "test-9632d.firebaseapp.com",
+    projectId: "test-9632d",
+    storageBucket: "test-9632d.appspot.com",
+    messagingSenderId: "496386485511",
+    appId: "1:496386485511:web:98ed5e418c1ce88ee929a2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessagingHandler(function(payload){
+messaging.setBackgroundMessageHandler(function (payload){
     console.log(payload);
 });
